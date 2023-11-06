eeac74025f645b155e189be1d65aa76e00ba316f
Peijmon Kasravi <kasravi.peijmon@gmail.com>
2023-11-06 11:39:47 -0800

    Added some template files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # blind-75
+
+This repository is meant to follow the famous Blind 75 list of LeetCode questions to study for technical interviews.
+
+The goal is to have well documented solutions for all of the problems in addition to some documentation for common patterns.
---
+++ cpp-template.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* -----------------------------------------------------------------------------
+ * Link: 
+ * 
+ * Solution Description:
+ *
+ * Steps:
+ *
+ * Complexity Analysis:
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+void my_solution() {
+	std::cout << "Hello world" << std::endl;
+}
+
