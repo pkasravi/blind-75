033dd353c752c70bbffe8ece6c1cf42045f995cc
pkasravi <kasravi.peijmon@gmail.com>
2024-01-09 16:28:31 -0800

    Changed a soln a bit

--- grind-75/dynamic-programming/maximum-subarray.py
+++ grind-75/dynamic-programming/maximum-subarray.py
@@ -1,15 +1,14 @@
 # ------------------------------------------------------------------------------
 # See cc version for notes
 # ------------------------------------------------------------------------------
-from typing import List
 
-def maxSubArray(self, nums: List[int]) -> int:
-    if len(nums) == 0:
-        return 0
+
+def maxSubArray(nums: List[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
 
     max_sum, running_sum = -float("inf"), 0
     for val in nums:
         running_sum = max(running_sum + val, val)
         max_sum = max(max_sum, running_sum)
     return max_sum
-
