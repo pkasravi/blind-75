4ad64a0f37d72847727ac3425bdf8bbf554ecba6
Peijmon Kasravi <kasravi.peijmon@gmail.com>
2025-06-29 15:03:30 -0700

    Flood fill cleanup

--- grind-75/graph/flood-fill.cc
+++ grind-75/graph/flood-fill.cc
@@ -3,7 +3,7 @@
 
 /* -----------------------------------------------------------------------------
  * Link: https://leetcode.com/problems/flood-fill/
- * 
+ *
  * Solution Description:
  * Starting from the starting cell perform a sort of BFS where we traverse the
  * matrix four directionally, and continue the BFS from any cells that get their
@@ -31,40 +31,42 @@ struct Point {
 };
 
 bool isValid(const Point& cell, const size_t rows, const size_t cols) {
-  return (cell.col >= 0) && (cell.col <= cols - 1) && (cell.row >= 0) && (cell.row <= rows - 1);
+  return (cell.col >= 0) && (cell.col <= cols - 1) && (cell.row >= 0) &&
+         (cell.row <= rows - 1);
 }
 
 std::vector<Point> getChildren(const Point& cell) {
   return {
-    {cell.row + 1, cell.col},
-    {cell.row - 1, cell.col},
-    {cell.row, cell.col + 1},
-    {cell.row, cell.col - 1},
+      {cell.row + 1, cell.col},
+      {cell.row - 1, cell.col},
+      {cell.row, cell.col + 1},
+      {cell.row, cell.col - 1},
   };
 }
 
-std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image, const int sr, const int sc, const int color) {
+std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image,
+                                        const int sr, const int sc,
+                                        const int color) {
   if (image[sr][sc] == color) {
     return image;
   }
   const size_t rows{image.size()};
   const size_t cols{image[0].size()};
   const int old_color{image[sr][sc]};
-  
-  std::queue<Point> work_queue{{{.row=sr, .col=sc}}};
+
+  std::queue<Point> work_queue{{{.row = sr, .col = sc}}};
 
   while (!work_queue.empty()) {
-      const auto curr{work_queue.front()};
-      work_queue.pop();
-      image[curr.row][curr.col] = color;
-      
-      for (const auto child : getChildren(curr)) {
-          if (
-							isValid(child, rows, cols) &&
-							image[child.row][child.col] == old_color) {
-              work_queue.push(child);
-          }
+    const auto curr{work_queue.front()};
+    work_queue.pop();
+    image[curr.row][curr.col] = color;
+
+    for (const auto child : getChildren(curr)) {
+      if (isValid(child, rows, cols) &&
+          image[child.row][child.col] == old_color) {
+        work_queue.push(child);
       }
+    }
   }
   return image;
 }
